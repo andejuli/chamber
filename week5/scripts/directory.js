bd4636120039e10c5ca64fa2df9961dc3155ff87
andejuli <techtrek@hotmail.com>
2023-03-06 09:57:17 -0700

    ham menu all pages

--- week5/scripts/directory.js
+++ week5/scripts/directory.js
@@ -125,3 +125,41 @@ function reportWindowSize() {
 
 window.addEventListener('resize', reportWindowSize);
 window.addEventListener('load', reportWindowSize);
+
+// Hamburger Menu
+
+const hambutton = document.querySelector('.ham');
+const mainnav = document.querySelector('.navigation')
+
+function toggleMenu(){
+    mainnav.classList.toggle('responsive');
+    hambutton.classList.toggle('responsive');
+}
+
+hambutton.addEventListener('click', toggleMenu);
+
+
+
+let w_names = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+let m_names = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+let mydate = new Date();
+console.log(mydate);
+
+let weekname = w_names[mydate.getDay()];
+let numDay = mydate.getDay();
+let monthname = m_names[mydate.getMonth()];
+
+const dateoutput = document.querySelector('#date');
+
+dateoutput.textContent = weekname + ", " + mydate.getDate() + " " + monthname + " " + mydate.getFullYear();
+
+
+//to show mon tues announcement message
+const parent = document.querySelector('#meeting');
+
+
+
+if (mydate.getDay() == 1 || mydate.getDay() == 2) {
+    mainnav.style.top = '180px';
+    parent.style.display = 'block';
+}
